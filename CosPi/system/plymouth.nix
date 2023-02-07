d5e12ad3999897e6f18e50f136d1e91e4f580dc5
creator54 <hi.creator54@gmail.com>
2023-02-07 23:09:32 +0530

    add some more kernel params for less boot messages

--- CosPi/system/plymouth.nix
+++ CosPi/system/plymouth.nix
@@ -25,7 +25,7 @@ let
 in
 {
   boot = {
-    kernelParams = [ "quiet" "splash" "vga=current" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "fbcon=nodefer" ];
+    kernelParams = [ "quiet" "loglevel=3" "systemd.show_status=false" "rd.systemd.show_status=false" "rd.udev.log_level=3" "splash" "vga=current" "udev.log_priority=3" "fbcon=nodefer" ];
     consoleLogLevel = 0;
     plymouth = {
       enable = true;
