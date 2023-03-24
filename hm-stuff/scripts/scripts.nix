865efbe2dd756db5b20fdc99b4d6bed818cc33f8
creator54 <hi.creator54@gmail.com>
2023-03-24 22:11:16 +0530

    add livewall

--- hm-stuff/scripts/scripts.nix
+++ hm-stuff/scripts/scripts.nix
@@ -1,9 +1,17 @@
 { config, pkgs, lib, ... }:
 let
   link = config.lib.file.mkOutOfStoreSymlink;
+
+  livewallLocal = /home/${(import ../../userConfig.nix).user}/livewall/livewall;
+  livewallSrc = if builtins.pathExists livewallLocal then builtins.readFile livewallLocal else link builtins.fetchurl{
+    url = "https://raw.githubusercontent.com/Creator54/livewall/main/livewall";
+    sha256 = "sha256:1v7jv0h04b6zq07lgl58gpmhlqx7j6k2rljddiw99hazkp0fl50l";
+  };
+
   c = pkgs.writeShellScriptBin "c" ''${link ./c.sh} $@'';
   gr = pkgs.writeShellScriptBin "gr" ''${link ./gr.sh} $@'';
   net = pkgs.writeShellScriptBin "net" ''${link ./net.sh} $@'';
+  livewall = pkgs.writeShellScriptBin "livewall" ''${livewallSrc}'';
   ram = pkgs.writeShellScriptBin "ram" ''${link ./ram.sh}'';
   cpu = pkgs.writeShellScriptBin "cpu" ''${link ./cpu.sh}'';
   fgit = pkgs.writeShellScriptBin "fgit" ''${link ./fgit.sh} $@'';
@@ -14,7 +22,7 @@ let
 in
 {
   home.packages = with pkgs; [
-    c gr net ram cpu fgit audio battery packages wifiInterface
+    c gr net ram cpu fgit audio livewall battery packages wifiInterface
   ];
 }
 
