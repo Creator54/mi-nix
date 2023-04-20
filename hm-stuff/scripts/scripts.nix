5a3811b31d54cb644056a6e3d2307170907b11e3
creator54 <hi.creator54@gmail.com>
2023-04-20 18:14:11 +0530

    fix : earlier $@ was getting splitted is some escape char was present, hence pass as a string

--- hm-stuff/scripts/scripts.nix
+++ hm-stuff/scripts/scripts.nix
@@ -8,16 +8,16 @@ let
     sha256 = "sha256:1v7jv0h04b6zq07lgl58gpmhlqx7j6k2rljddiw99hazkp0fl50l";
   };
 
-  c = pkgs.writeShellScriptBin "c" ''${link ./c.sh} $@'';
-  v = pkgs.writeShellScriptBin "v" ''${link ./v.sh} $@'';
-  gr = pkgs.writeShellScriptBin "gr" ''${link ./gr.sh} $@'';
-  net = pkgs.writeShellScriptBin "net" ''${link ./net.sh} $@'';
-  livewall = pkgs.writeShellScriptBin "livewall" ''${livewallSrc} $@'';
+  c = pkgs.writeShellScriptBin "c" ''${link ./c.sh} "$@"'';
+  v = pkgs.writeShellScriptBin "v" ''${link ./v.sh} "$@"'';
+  gr = pkgs.writeShellScriptBin "gr" ''${link ./gr.sh} "$@"'';
+  net = pkgs.writeShellScriptBin "net" ''${link ./net.sh} "$@"'';
+  livewall = pkgs.writeShellScriptBin "livewall" ''${livewallSrc} "$@"'';
   ram = pkgs.writeShellScriptBin "ram" ''${link ./ram.sh}'';
   cpu = pkgs.writeShellScriptBin "cpu" ''${link ./cpu.sh}'';
-  fgit = pkgs.writeShellScriptBin "fgit" ''${link ./fgit.sh} $@'';
-  audio = pkgs.writeShellScriptBin "audio" ''${link ./audio.sh} $@'';
-  battery = pkgs.writeShellScriptBin "battery" ''${link ./battery.sh} $@'';
+  fgit = pkgs.writeShellScriptBin "fgit" ''${link ./fgit.sh} "$@"'';
+  audio = pkgs.writeShellScriptBin "audio" ''${link ./audio.sh} "$@"'';
+  battery = pkgs.writeShellScriptBin "battery" ''${link ./battery.sh} "$@"'';
   packages = pkgs.writeShellScriptBin "packages" ''${link ./packages.sh}'';
   wifiInterface = pkgs.writeShellScriptBin "wifiInterface" ''ip a | grep wlp | cut -d':' -f2| head -n1 |xargs'';
 in
