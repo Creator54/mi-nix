ed97dba06fc2d319f7b2e2965777349c32f418a1
creator54 <hi.creator54@gmail.com>
2023-06-02 19:43:55 +0530

    cleanup

--- CosPi/system/hosts.nix
+++ CosPi/system/hosts.nix
@@ -1,16 +1,8 @@
 {
   networking.hostFiles = [
     (builtins.fetchurl {
-      url = "https://adaway.org/hosts.txt";
-      sha256 = "sha256:10jrncass5nc07lr3dlziscmfk6wm43wiyvp4zmrs7zlnimj97cs";
-    })
-    (builtins.fetchurl {
-      url = "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt";
-      sha256 = "sha256:1bcicm9g14b60qyiy3awxxwh3f280vvfl8aprpi1g1irjgiy6ag2";
-    })
-    (builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
-      sha256 = "sha256:0y4sghzlnv0gyqdqf20c0la1y11fbs0ydynvq76kk68f0ppv47c1";
+      url = "https://raw.githubusercontent.com/StevenBlack/hosts/8007e17d0a66bc9f43d79bb82a218f253d28797a/hosts";
+      sha256 = "sha256:0na31fzvm5jh2c1rh260ghq61fsaqdfqmi0n7nbhjzpmcd23wx6z";
     })
     (builtins.fetchurl {
       url = "https://raw.githubusercontent.com/creator54/youtube_ad_blocklist/master/blocklist.txt";
--- hm-stuff/pkgs/firefox.nix
+++ hm-stuff/pkgs/firefox.nix
@@ -12,7 +12,10 @@ let
 in
 {
   nixpkgs.config.packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+    nur = import (builtins.fetchTarball{
+      url = "https://github.com/nix-community/NUR/archive/d20910e4325043886e32c8d459b727e1e5be079e.tar.gz";
+      sha256 = "sha256:0rzcslnig64flpb4hyxlpbdjnyl0k3aygb3y78yk0klraqrm2vis";
+      }) {
       inherit pkgs;
     };
   };
@@ -43,32 +46,32 @@ in
         userChrome = builtins.readFile userChromeCSS;
         settings = { #about:config
         "browser.startup.homepage" = "${startPage}";
-          "browser.search.region" = "IN";
-          "browser.search.isUS" = false;
-          "browser.download.animateNotifications" = false;
-          "security.dialog_enable_delay" = 0;
-          "distribution.searchplugins.defaultLocale" = "en-IN";
-          "general.useragent.locale" = "en-IN";
-          "browser.bookmarks.showMobileBookmarks" = true;
-          "ui.systemUsesDarkTheme" = 1;
-          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-          "svg.context-properties.content.enabled" = true;
-          "layers.acceleration.force-enabled" = true;
-          "gfx.webrender.all" = true;
-          "extensions.webextensions.restrictedDomains" = ""; #enable adding custom css even on restricted/firefox sites.
-          "browser.newtabpage.activity-stream.feeds.telemetry" = false;
-          "browser.newtabpage.activity-stream.telemetry" = false;
-          "browser.ping-centre.telemetry" = false;
-          "toolkit.telemetry.archive.enabled" = false;
-          "toolkit.telemetry.bhrPing.enabled" = false;
-          "toolkit.telemetry.enabled" = false;
-          "toolkit.telemetry.firstShutdownPing.enabled" = false;
-          "toolkit.telemetry.hybridContent.enabled" = false;
-          "toolkit.telemetry.newProfilePing.enabled" = false;
-          "toolkit.telemetry.reportingpolicy.firstRun" = false;
-          "toolkit.telemetry.shutdownPingSender.enabled" = false;
-          "toolkit.telemetry.unified" = false;
-          "toolkit.telemetry.updatePing.enabled" = false;
+        "browser.search.region" = "IN";
+        "browser.search.isUS" = false;
+        "browser.download.animateNotifications" = false;
+        "security.dialog_enable_delay" = 0;
+        "distribution.searchplugins.defaultLocale" = "en-IN";
+        "general.useragent.locale" = "en-IN";
+        "browser.bookmarks.showMobileBookmarks" = true;
+        "ui.systemUsesDarkTheme" = 1;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "svg.context-properties.content.enabled" = true;
+        "layers.acceleration.force-enabled" = true;
+        "gfx.webrender.all" = true;
+        "extensions.webextensions.restrictedDomains" = ""; #enable adding custom css even on restricted/firefox sites.
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+        "browser.newtabpage.activity-stream.telemetry" = false;
+        "browser.ping-centre.telemetry" = false;
+        "toolkit.telemetry.archive.enabled" = false;
+        "toolkit.telemetry.bhrPing.enabled" = false;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.firstShutdownPing.enabled" = false;
+        "toolkit.telemetry.hybridContent.enabled" = false;
+        "toolkit.telemetry.newProfilePing.enabled" = false;
+        "toolkit.telemetry.reportingpolicy.firstRun" = false;
+        "toolkit.telemetry.shutdownPingSender.enabled" = false;
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.updatePing.enabled" = false;
         };
       };
     };
