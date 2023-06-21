3508d62f8743ceb212b900cc4eda88ce72adbaa6
creator54 <hi.creator54@gmail.com>
2023-06-22 01:33:18 +0530

    add startship config

--- hm-stuff/configs/symlinks.nix
+++ hm-stuff/configs/symlinks.nix
@@ -11,6 +11,7 @@ in
     ".config/xplr".source                                 = link ./xplr;
     ".config/mpv/scripts".source                          = link ./mpv/scripts;
     ".config/mpv/script-opts/youtube-quality.conf".source = link ./mpv/youtube-quality.conf;
+    ".config/starship.toml".source                        = link ./starship.toml;
     ".config/default.png".source                          = link ./default.png;
     ".config/gtk-3.0".source                              = link ./gtk-3.0;
     ".config/htop".source                                 = link ./htop;
