bd96900f0e9bac0d8de149ad584e10982a24aa4e
creator54 <hi.creator54@gmail.com>
2023-11-06 01:31:24 +0530

    switch to rsa, else ssh auth resets github

--- hm-stuff/configs/fish/functions/ssh-setup.fish
+++ hm-stuff/configs/fish/functions/ssh-setup.fish
@@ -2,7 +2,7 @@ function ssh-setup
     set ids_loc "/home/creator54/.ssh/id_rsa_"
     read -P "Email: " email
     set ids_loc {$ids_loc}(echo $email | cut -d'@' -f2 | cut -d'.' -f1) #pass ORG name as unique identifier. i.e string b/w '@' & '.' from mail-id
-    echo $ids_loc | ssh-keygen -t ed25519 -C $email &>/dev/null
+    echo $ids_loc | ssh-keygen -t rsa -b 4096 -C $email &>/dev/null
     eval (ssh-agent -c)
     ssh-add $ids_loc
     cat $ids_loc.pub | clip
