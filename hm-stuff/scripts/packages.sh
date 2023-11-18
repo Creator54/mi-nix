aa4917e3170882bda028982b3d7c4eb710b1e2ae
creator54 <hi.creator54@gmail.com>
2023-11-19 01:25:23 +0530

    refactor: packages.sh

--- hm-stuff/scripts/packages.sh
+++ hm-stuff/scripts/packages.sh
@@ -1,17 +1,33 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e -o pipefail
 
-package=$((ls ~/.nix-profile/bin/;ls ~/.config/fish/functions/;ls ~/.config/fish/scripts/|grep -v "README.md") | dmenu -p " Packages :")
+# Function to list unique executables in a directory
+list_executables() {
+	ls -1 "$1" 2>/dev/null | awk '!seen[$0]++'
+}
 
-if [[ $package =~ ".fish" ]] || [[ $package =~ ".sh" ]];then
-  package=$(echo $package|sed 's/.fish//; s/.sh//') #remove all .fish/.sh
-  if [[ $package == "cdev" ]]; then
-    exec "$package" &
-  else
-    $TERMINAL -e $SHELL -c "$package;read -P \n'Press Enter to exit !!' key"
-  fi
-else
-  exec $package &
+# List unique executables in directories from $PATH and additional locations
+package=$( (
+	IFS=: read -ra PATH_DIRS <<<"$PATH"
+	for dir in "${PATH_DIRS[@]}"; do
+		list_executables "$dir"
+	done
+	list_executables ~/.config/fish/functions/
+	list_executables ~/.config/fish/scripts/ | grep -v "README.md"
+) | dmenu -p "Packages:")
+
+if [ -n "$package" ]; then
+	if [[ $package =~ ".fish" ]] || [[ $package =~ ".sh" ]]; then
+		package=$(echo $package | sed 's/.fish//; s/.sh//') # remove .fish/.sh extensions
+		if [[ $package == "cdev" ]]; then
+			exec "$package" &
+		else
+			$TERMINAL -e $SHELL -c "$package; read -p 'Press Enter to exit!!' key"
+		fi
+	else
+		exec "$package" &
+	fi
 fi
+
 exit 0
