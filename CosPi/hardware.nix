44c51a1dbd71667139c914312f4638e7172c6769
creator54 <hi.creator54@gmail.com>
2023-11-18 22:35:05 +0530

    cleanup

--- CosPi/hardware.nix
+++ CosPi/hardware.nix
@@ -13,7 +13,6 @@
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
