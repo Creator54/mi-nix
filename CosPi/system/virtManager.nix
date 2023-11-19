3f7e99195f326f27f0dcd8f456b6676ef70bbafa
creator54 <hi.creator54@gmail.com>
2023-11-19 18:25:29 +0530

    include VM's

--- CosPi/system/virtManager.nix
+++ CosPi/system/virtManager.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ virt-manager virt-viewer ];
+  environment.systemPackages = with pkgs; [ virt-manager virt-viewer quickemu ];
   users.users.creator54.extraGroups = [ "libvirtd" ];
   virtualisation.libvirtd.enable = true;
 }
--- hm-stuff/scripts/packages.sh
+++ hm-stuff/scripts/packages.sh
@@ -8,25 +8,33 @@ list_executables() {
 }
 
 # List unique executables in directories from $PATH and additional locations
-package=$( (
+selected_package=$( (
 	IFS=: read -ra PATH_DIRS <<<"$PATH"
 	for dir in "${PATH_DIRS[@]}"; do
 		list_executables "$dir"
 	done
 	list_executables ~/.config/fish/functions/
 	list_executables ~/.config/fish/scripts/ | grep -v "README.md"
+	echo "windows11"
+	echo "macos-catalina"
 ) | dmenu -p "Packages:")
 
-if [ -n "$package" ]; then
-	if [[ $package =~ ".fish" ]] || [[ $package =~ ".sh" ]]; then
-		package=$(echo $package | sed 's/.fish//; s/.sh//') # remove .fish/.sh extensions
-		if [[ $package == "cdev" ]]; then
-			exec "$package" &
+if [ -n "$selected_package" ]; then
+	if [[ $selected_package == "windows11" ]]; then
+		cd /home/$USER/VMS/
+		quickemu --vm windows-11.conf --screen 0
+	elif [[ $selected_package == "macos-catalina" ]]; then
+		cd /home/$USER/VMS/
+		quickemu --vm macos-catalina.conf --screen 0
+	elif [[ $selected_package =~ \.fish$|\.sh$ ]]; then
+		selected_package=$(echo "$selected_package" | sed 's/.fish//; s/.sh//') # remove .fish/.sh extensions
+		if [[ $selected_package == "cdev" ]]; then
+			exec "$selected_package" &
 		else
-			$TERMINAL -e $SHELL -c "$package; read -p 'Press Enter to exit!!' key"
+			$TERMINAL -e $SHELL -c "$selected_package; read -p 'Press Enter to exit!!' key"
 		fi
 	else
-		exec "$package" &
+		exec "$selected_package" &
 	fi
 fi
 
