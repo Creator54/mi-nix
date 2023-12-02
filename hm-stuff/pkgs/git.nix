d8d8149f13a941a46e32d61b9260b9e7dad349cb
creator54 <hi.creator54@gmail.com>
2023-12-02 15:51:28 +0530

    setup git profiles

--- hm-stuff/pkgs/git.nix
+++ hm-stuff/pkgs/git.nix
@@ -8,6 +8,20 @@ in
       enable = true;
       userName = "${uc.user}";
       userEmail = "${uc.userEmail}";
+      extraConfig = {
+        pull.rebase = true;
+      };
+      # ref:
+      # https://discourse.nixos.org/t/is-it-possible-to-change-the-default-git-user-config-for-a-devshell/17612/3
+      # https://github.com/NobbZ/nixos-config/blob/8848aa0cc4d65d7960ec2c8535e33d212e6691d2/home/modules/profiles/development/default.nix#L70-L76
+      includes = [{
+        condition = "gitdir:~/Work/**";
+        contents = {
+          init.defaultBranch = "master";
+          user.name = "Saroj Kumar Mahato";
+          user.email = "sarojkumar@criodo.com";
+        };
+      }];
     };
     lazygit.enable = true;
   };
