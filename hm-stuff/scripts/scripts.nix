4e4b2a2e4a2750cadfdf8603794ab30078e44960
creator54 <hi.creator54@gmail.com>
2023-12-03 16:12:20 +0530

    add: ghv

--- hm-stuff/scripts/scripts.nix
+++ hm-stuff/scripts/scripts.nix
@@ -11,18 +11,27 @@ let
     });
   }));
 
+  ghvSrc = pkgs.writeShellScript "ghv" (builtins.readFile (builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/Creator54/ghv/main/ghv";
+    sha256 = builtins.hashFile "sha256" (builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/Creator54/ghv/main/ghv";
+      name = "ghv";
+    });
+  }));
+
 
   c = pkgs.writeShellScriptBin "c" ''${link ./c.sh} "$@"'';
   v = pkgs.writeShellScriptBin "v" ''${link ./v.sh} "$@"'';
   gr = pkgs.writeShellScriptBin "gr" ''${link ./gr.sh} "$@"'';
   bt = pkgs.writeShellScriptBin "bt" ''${link ./bt.sh}'';
   net = pkgs.writeShellScriptBin "net" ''${link ./net.sh} "$@"'';
-  livewall = pkgs.writeShellScriptBin "livewall" ''${livewallSrc} "$@"'';
+  ghv = pkgs.writeShellScriptBin "ghv" ''${ghvSrc} "$@"'';
   ram = pkgs.writeShellScriptBin "ram" ''${link ./ram.sh}'';
   cpu = pkgs.writeShellScriptBin "cpu" ''${link ./cpu.sh}'';
   fgit = pkgs.writeShellScriptBin "fgit" ''${link ./fgit.sh} "$@"'';
   swap = pkgs.writeShellScriptBin "swap" ''${link ./swap.sh} "$@"'';
   audio = pkgs.writeShellScriptBin "audio" ''${link ./audio.sh} "$@"'';
+  livewall = pkgs.writeShellScriptBin "livewall" ''${livewallSrc} "$@"'';
   maxzip = pkgs.writeShellScriptBin "maxzip" ''${link ./maxzip.sh} "$@"'';
   battery = pkgs.writeShellScriptBin "battery" ''${link ./battery.sh} "$@"'';
   wificonnect = pkgs.writeShellScriptBin "wificonnect" ''${link ./wificonnect.sh} "$@"'';
@@ -31,7 +40,7 @@ let
 in
 {
   home.packages = with pkgs; [
-    c v bt gr net ram cpu fgit swap maxzip audio battery packages wificonnect livewall wifiInterface
+    c v bt gr ghv net ram cpu fgit swap maxzip audio battery packages wificonnect livewall wifiInterface
   ];
 }
 
