5ade28d8ac95a97db896fdb2c6845e21c74a2a89
creator54 <hi.creator54@gmail.com>
2023-12-16 14:03:31 +0530

    fix: BT issues -       few still remainig ig

--- CosPi/system/bt.nix
+++ CosPi/system/bt.nix
@@ -2,7 +2,7 @@
 
 let
   bluez = pkgs.bluezFull.overrideAttrs (oldAttrs: {
-    configureFlags = oldAttrs.configureFlags ++ [ "--enable-experimental" ];
+    configureFlags = oldAttrs.configureFlags ++ [ "--enable-experimental"];
   });
 in
 
@@ -13,6 +13,17 @@ in
       enable = true;
       package = bluez;
       powerOnBoot = true;
+      settings = {
+        General = {
+          Name = "Hello";
+          ControllerMode = "dual";
+          FastConnectable = "true";
+          Experimental = "true";
+        };
+        Policy = {
+          AutoEnable = "true";
+        };
+      };
     };
   };
 
@@ -25,6 +36,18 @@ in
   };
 
   systemd.services = {
+    bluetooth = {
+      description = "Bluetooth service";
+      serviceConfig = {
+        Type = "dbus";
+        BusName = "org.bluez";
+        ExecStart = [
+          ""
+          "${pkgs.bluez}/bin/bluetoothd --noplugin=sap,vcp,mcp,bap"
+        ];
+      };
+      wantedBy = [ "bluetooth.target"  ];
+    };
     bluetoothUnblock = {
       description = "Unblock Bluetooth on startup";
       serviceConfig = {
