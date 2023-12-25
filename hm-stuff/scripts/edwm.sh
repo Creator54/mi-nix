0f8f0992c927a1fc76500771f6a94b412b6fd13e
creator54 <hi.creator54@gmail.com>
2023-12-25 16:22:21 +0530

    refactoring

--- hm-stuff/scripts/edwm.sh
+++ hm-stuff/scripts/edwm.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
 
 dwm_dir=~/dwm
+kittyConfig=(
+	"layout tall:bias=70"
+	""
+	"# first kitty window"
+	"launch --title \"Source\" vim config.def.h"
+	""
+	"# second kitty window"
+	"launch --title \"Compilation Output\" bash -c \"ls config.def.h | entr nix-shell --run 'make clean;make'\""
+)
 
 if [[ -d "$dwm_dir" ]]; then
 	cd "$dwm_dir" || exit
 
 	configFile=$(mktemp)
 
-	read -r -d '' kittyConfig <<EOF
-  layout tall:bias=70
-
-  # first kitty window
-  launch --title "Source" vim config.def.h
-
-  # second kitty window
-  launch --title "Compilation Output" bash -c "ls config.def.h | entr nix-shell --run 'make clean;make'"
-EOF
-	echo "$kittyConfig" >"$configFile"
+	printf "%s\n" "${kittyConfig[@]}" >"$configFile"
 
 	kitty --session "$configFile"
-	rm "$configFile"
 else
 	echo "Local Dwm not found at $dwm_dir"
 fi
